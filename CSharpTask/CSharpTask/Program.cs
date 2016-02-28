7bcac833b0467e9c048b8a320343ece2bd25ebcc
JerryChou <Jerry_Chou_123@163.com>
2016-02-28 10:25:38 +0800

    完成Task的初步学习

--- CSharpTask/CSharpTask/Program.cs
+++ CSharpTask/CSharpTask/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CSharpTask
 {
@@ -9,8 +11,70 @@ namespace CSharpTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+
+            //Task t1 = Task.Factory.StartNew(delegate { MyMethodA(); });
+            //Task t2 = Task.Factory.StartNew(delegate { MyMethodB(); });
+
+            //t1.Wait();
+            //t2.Wait();
+
+
+            //Task.WaitAll(t1, t2);
+
+            //Console.WriteLine("Hello world!");
+
+
+
+            //Task t1 = new Task(MyMethod);
+            //t1.Start();
+
+            //Console.WriteLine("主线程代码运行结束");
+
+
+            //Task t1 = new Task(F1);
+            //t1.Start();
+            ////t1.Wait();
+
+            Task<string> t1 = Task.Factory.StartNew(() => "测试");
+            t1.Wait();
+            Console.WriteLine(t1.Result);
+
+
+            Console.WriteLine("Done");
             Console.ReadKey();
+        }
+
+        private static void F1()
+        {
+            throw new NotImplementedException();
+        }
+
+        static void MyMethod()
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(DateTime.Now.ToString());
+                Thread.Sleep(1000);
+            }
+        }
+
+
+
+        private static int n = 20;
+        private static void MyMethodA()
+        {
+            for (int i = 1; i <= n; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static void MyMethodB()
+        {
+            for (int i = 2; i <= n; i += 2)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
